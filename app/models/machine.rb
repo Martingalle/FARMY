2c772403729e5d00b6156e93f4329b2d98619b80
Anthony Lionnet <lionnet.ant@gmail.com>
2018-08-20 17:02:32 +0200

    git init

---
+++ app/models/machine.rb
@@ -0,0 +1,4 @@
+class Machine < ApplicationRecord
+  has_many :bookings
+  belongs_to :user
+end
---
+++ db/migrate/20180820143810_create_machines.rb
@@ -0,0 +1,11 @@
+class CreateMachines < ActiveRecord::Migration[5.2]
+  def change
+    create_table :machines do |t|
+      t.string :name
+      t.string :location
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
