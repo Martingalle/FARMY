f6d070e111af4f74545450a2cad3e4b3511aceaf
Anthony Lionnet <lionnet.ant@gmail.com>
2018-08-20 18:23:59 +0200

    Adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,9 @@
 Rails.application.routes.draw do
-  get 'bookings/index'
-  get 'bookings/new'
-  get 'bookings/create'
-  get 'bookings/destroy'
-  get 'machines/index'
-  get 'machines/show'
-  get 'machines/new'
-  get 'machines/create'
-  get 'machines/edit'
-  get 'machines/update'
-  get 'machines/destroy'
   devise_for :users
   resources :machines do
-    resources :bookings, only: [:index, :show, :create]
+    resources :bookings, only: [:new, :create]
   end
-  resources :bookings, only: [:destroy]
+  resources :bookings, only: [:index, :show, :destroy]
   root to: "machines#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
