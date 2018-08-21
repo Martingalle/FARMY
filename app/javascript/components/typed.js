615837cd4c2ae83da7c0978209faaa647905b582
Anthony Lionnet <lionnet.ant@gmail.com>
2018-08-21 16:19:02 +0200

    💎: Adding how to strate to homepage

--- app/javascript/components/typed.js
+++ app/javascript/components/typed.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js';
 function loadDynamicBannerText() {
   new Typed('#type', {
     strings: ["Tractor", "Planter", "Fodder"],
-    typeSpeed: 80,
+    typeSpeed: 90,
     loop: true
   });
 }
