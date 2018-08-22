ce77b83882baf4f285a8ec37d3397a5d2979f424
Thomas Bromehead <t_bromehead@yahoo.fr>
2018-08-22 17:30:12 +0200

    Created User Dashboard

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -45,7 +45,7 @@ class MachinesController < ApplicationController
   def list
     @current_user = current_user
     @current_user_id = current_user.id
-    # @myMachines = Machine.where({|machine| machine.user_id == current_user})
+
   end
 
   private
