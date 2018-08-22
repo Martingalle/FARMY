af5f326ce21efe821c48047daae0b8f3755c917e
Thomas Bromehead <t_bromehead@yahoo.fr>
2018-08-22 12:44:00 +0200

    Added Cloudinary Pics to the Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,41 +19,39 @@ end
 puts 'created all users'
 puts 'creating machines'
 
+makes = ["Massey-Ferguson", "New Holland", "International", "Bobard", "McCormick", "Kobatsu", "Manitou", "John Deere", "Deutz-Fahr", "Someca"]
+cities = ["Nevers", "Bourgoin-Jallieu", "Dinard", "Auxerre", "Nantua", "Cassis", "Romorantin-Lanthenay", "Dinard", "Colmar", "Dunkerque" ]
+
 10.times do |machine|
-  makes = ["Massey-Ferguson", "New Holland", "International", "Bobard", "McCormick", "Kobatsu", "Manitou", "John Deere", "Deutz-Fahr", "Someca"]
   make = makes.sample
-  makes.delete(make)
-  cities = ["Nevers", "Bourgoin-Jallieu", "Dinard", "Auxerre", "Nantua", "Cassis", "Romorantin-Lanthenay", "Dinard", "Colmar", "Dunkerque" ]
   location = cities.sample
-  cities.delete(location)
   year = rand(1990..2018)
-  categories = ["Tractor","Combine Harvester","Sower","Sprayer","Shredder","Spreader","Rolls","Sidewinder","Vineyard tractor"]    
+  category = ["Tractor","Combine Harvester","Sower","Sprayer","Shredder","Spreader","Rolls","Sidewinder","Vineyard tractor"].sample    
   roues_motrices = [2,4].sample
-  category = categories.sample
   case category
     when "Tractor"
-      photo = ["kwcl2tfxqdfb13quybr","imvxgrkpf2p8byiwsrs"].sample
-      photo.delete(photo)
+      photo = ["v1534854305/kwcl2ltfxqdfb13quybr","v1534854269/imvxvgrkpf2p8byiwsrs"].sample
     when "Combine Harvester"
-      photo = ["vuqbgu4e8knh6xdprfrn"]
+      photo = "v1534854342/vuqbgu4e8knh6xdprfrn"
     when "Sower"
-      photo = ["enpaiffh1vtlfdgpgwpz"]
+      photo = "v1534854191/enpaiffh1vtlfdgpgwpz"
     when "Sprayer"
-      photo = ["nve3asouppu4vldf62tn"]
+      photo = "v1534854627/nve3asouppu4vldf62tn"
     when "Shredder"
-      photo = ["huarkiih1lilpn7xiabv"]
+      photo = "huarkiih1lilpn7xiabv"
     when "Spreader"
-      photo = ["qzqnyzle6pqot12f55eq"]
+      photo = "v1534854727/qzqnyzle6pqot12f55eq"
     when "Rolls"
-      photo = ["j55tpqlilxtqecurqqoz"]
+      photo = "v1534854620/j55tpqlilxtqecurqqoz"
     when "Sidewinder"
-      photo = ["glmjyatp0zzd5qbkq2hv"]
+      photo = "v1534854279/glmjyatp0zzd5qbkq2hv"
     when "Vineyard Tractor"
-      photo = ["nhcvwhnu7kmwhnv6czns"]
+      photo = "v1534853673/nhcvwhnu7kmwhnv6czns"
+    end
   ids = User.pluck(:id)
   force_moteur = rand(50..400)
   price= rand(20..200)
-  machine = Machine.create!(make: make, location: location, user_id: ids.sample, year:year, category:category, force_moteur:force_moteur, price_per_hour:price, photo:photo)
+  machine = Machine.create!(make: make, location: location, user_id: ids.sample, year:year, category:category, force_moteur:force_moteur, price_per_hour:price, remote_photo_url: "https://res.cloudinary.com/ehrsatz/image/upload/#{photo}.jpg")
 end
 
 puts 'finished creating the machines'
