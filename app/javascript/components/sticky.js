258a855fadcbba197593eb7d9800a50b3e7036d9
Anthony Lionnet <lionnet.ant@gmail.com>
2018-08-22 09:21:27 +0200

    Sticky improvment

--- app/javascript/components/sticky.js
+++ app/javascript/components/sticky.js
@@ -5,10 +5,10 @@ function stickyNavigation() {
     const header = document.getElementById('navigation');
     const masternav = document.getElementById('headnav');
     const header_height = 200;
-    const add_class_on_scroll = () => header.classList.add("fade-in")
-    const add_class_on_scroll_master = () => masternav.classList.add("stick")
-    const remove_class_on_scroll = () => header.classList.remove("fade-in")
-    const remove_class_on_scroll_master = () => masternav.classList.remove("stick")
+    const add_class_on_scroll = () => header.classList.add("fade-in");
+    const add_class_on_scroll_master = () => masternav.classList.add("stick");
+    const remove_class_on_scroll = () => header.classList.remove("fade-in");
+    const remove_class_on_scroll_master = () => masternav.classList.remove("stick");
     window.addEventListener('scroll', function() {
       scrollpos = window.scrollY;
       if (scrollpos >= header_height) { add_class_on_scroll(); add_class_on_scroll_master()  }
