3fb20e357f4483acd42e7d0497f389740cec81c0
Anthony Lionnet <lionnet.ant@gmail.com>
2018-08-24 16:15:22 +0200

    Styling sweetalert

--- app/javascript/components/sweetalert.js
+++ app/javascript/components/sweetalert.js
@@ -4,7 +4,18 @@ function bindSweetAlertButtonDemo() {
   const swalButton = document.getElementById('delete');
   if (swalButton) { // protect other pages
     swalButton.addEventListener('click', () => {
-      swal("Hello world!");
+      swal({
+        title: 'Cathy Tuche is deleting your marchin',
+        width: 600,
+        padding: '3em',
+        background: '#fff url(/images/trees.png)',
+        backdrop: `
+          rgba(0,0,123,0.4)
+          url('./images/nyan-cat.gif')
+          center left
+          no-repeat
+        `
+      })
     });
   }
 }
